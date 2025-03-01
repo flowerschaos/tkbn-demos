b7a034c06fdce863d17e4b3314aed9d7a7c03337
flowerschaos <cfactionbackup@gmail.com>
2025-03-01 12:36:10 -0500

    changing stat panel stuff so it doesnt bother me

--- statpanel.gd
+++ statpanel.gd
@@ -4,7 +4,7 @@ extends Panel
 @onready var ap: Label = $stats/ap
 
 func _on_ap_change(value: Variant) -> void:
-	ap.text = "AP\n"+str(value)
+	ap.text = "AP: "+str(value)
 
 func _on_hp_change(value: Variant) -> void:
-	hp.text = "HP\n"+str(value)
+	hp.text = "HP: "+str(value)
