5abd73da7dde4a8f18a17edc38fae0631841fa53
flowerschaos <cfactionbackup@gmail.com>
2025-03-07 02:15:11 -0500

    hud updated ish

--- combat/buttons/actionbutton.gd
+++ combat/buttons/actionbutton.gd
@@ -1,8 +1,7 @@
 extends Button
 class_name ActionButton
-@onready var main = get_tree().current_scene
-@onready var enemy = main.get_node("enemy")
-@onready var player = main.get_node("player")
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var enemy = get_tree().get_first_node_in_group("enemy")
 
 func _on_pressed() -> void:
 	if enemy and player:
--- combat/combat_manager.gd
+++ combat/combat_manager.gd
@@ -1,16 +1,18 @@
 extends Node
-@onready var player: Player = $player
-@onready var enemy: Enemy = $enemy
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var enemy = get_tree().get_first_node_in_group("enemy")
 @onready var combat_ui: Control = $"combat ui"
 @onready var playeractions: VBoxContainer = $"combat ui/actions"
 @onready var playerstatpanel: PanelContainer = $"combat ui/playerstats"
+@onready var hp_label: Label = $"combat ui/playerstats/HBoxContainer/hp"
+@onready var ap_label: Label = $"combat ui/playerstats/HBoxContainer/ap"
 @onready var playercam: PhantomCamera3D = $playercam
 @onready var enemycam: PhantomCamera3D = $enemycam
 
 func _ready() -> void:
 	signals.connect("enter_combat", _on_combat_enter)
-	player.connect("ap_changed", playerstatpanel._on_ap_changed)
-	player.connect("hp_changed", playerstatpanel._on_hp_changed)
+	player.connect("ap_changed", _on_ap_changed)
+	player.connect("hp_changed", _on_hp_changed)
 
 func _on_combat_enter():
 	enemy.connect("on_death", _on_enemy_death)
@@ -42,3 +44,9 @@ func start_player_turn():
 func _on_enemy_death() -> void:
 	enemy = null
 	print("enemy gone")
+
+# handling ui
+func _on_ap_changed(value: Variant) -> void:
+	ap_label.text = "ap: "+str(value)
+func _on_hp_changed(value: Variant) -> void:
+	hp_label.text = "hp: "+str(value)
