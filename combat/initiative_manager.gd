24d8cb91bf0ceb9662034ae4fde5ebd3a4183310
flowerschaos <cfactionbackup@gmail.com>
2025-03-07 21:01:43 -0500

    worst beat that's ever been made

--- combat/initiative_manager.gd
+++ combat/initiative_manager.gd
@@ -5,18 +5,13 @@ class_name InitiativeManager
 @onready var actors: Array = get_tree().get_nodes_in_group("actor")
 
 func speed_sort(a, b):
-	if a.speed < b.speed:
+	if a.speed > b.speed:
 		return true
 	return false
 
-func play_turn():
-	await active_character.play_turn
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	actors.sort_custom(speed_sort)
 	print(actors)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
