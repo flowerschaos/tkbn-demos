11416f89bd8b079cc30640490cf6be957b83c802
flowerschaos <cfactionbackup@gmail.com>
2025-03-10 08:20:43 -0400

    implementing turn queue properly

--- combat/initiative_manager.gd
+++ combat/initiative_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var actors: Array = get_tree().get_nodes_in_group("actor")
+var turn_order: Array = []
 @onready var timeline: HBoxContainer = $"../ui container/combat ui/timeline"
 
 signal end_turn
@@ -9,10 +10,12 @@ func speed_sort(a, b):
 	return a.speed > b.speed
 
 func _ready() -> void:
+	end_turn.connect(_on_turn_end())
 	actors.sort_custom(speed_sort)
+	turn_order = actors.duplicate()
 
 func _process(delta: float) -> void:
 	pass
 
 func _on_turn_end():
-	pass
+	var current_actor = turn_order.pop_front()
