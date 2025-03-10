df7088f4a0bbf713e0e68260406fae750ff893f9
flowerschaos <cfactionbackup@gmail.com>
2025-03-09 20:25:21 -0400

    crying

--- combat/combat_manager.gd
+++ combat/combat_manager.gd
@@ -1,7 +1,6 @@
 extends Node
-@onready var player = get_tree().get_nodes_in_group("player")
-@onready var enemy = get_tree().get_nodes_in_group("enemy")
-@onready var combat_ui: Control = $"combat ui"
+@onready var actors = get_tree().get_nodes_in_group("actor")
+@onready var combat_ui: Control = $"ui container/combat ui"
 @onready var playeractions: VBoxContainer = $"combat ui/actions"
 @onready var playerstatpanel: PanelContainer = $"combat ui/playerstats"
 @onready var hp_label: Label = $"combat ui/playerstats/HBoxContainer/hp"
@@ -11,38 +10,6 @@ extends Node
 
 func _ready() -> void:
 	signals.connect("enter_combat", _on_combat_enter)
-	player.connect("ap_changed", _on_ap_changed)
-	player.connect("hp_changed", _on_hp_changed)
 
-func _on_combat_enter():
-	enemy.connect("on_death", _on_enemy_death)
+func _on_combat_enter():	
 	print("combat begin!")
-
-func start_enemy_turn():
-	enemycam.priority = 1
-	playercam.priority = 0
-	playeractions.hide()
-	if enemy:
-		enemy.attack(player)
-		print("player attacked")
-		await enemy.end_turn
-	start_player_turn()
-
-func start_player_turn():
-	enemycam.priority = 0
-	playercam.priority = 1
-	playeractions.show()
-	playerstatpanel.show()
-	player.ap = player.max_ap
-	await player.end_turn
-	start_enemy_turn()
-
-func _on_enemy_death() -> void:
-	enemy = null
-	print("enemy gone")
-
-# handling ui
-func _on_ap_changed(value: Variant) -> void:
-	ap_label.text = "ap: "+str(value)
-func _on_hp_changed(value: Variant) -> void:
-	hp_label.text = "hp: "+str(value)
--- combat/initiative_manager.gd
+++ combat/initiative_manager.gd
@@ -2,13 +2,13 @@ extends Node
 class_name InitiativeManager
 
 @onready var actors: Array = get_tree().get_nodes_in_group("actor")
+@onready var timeline: HBoxContainer = $"../ui container/combat ui/timeline"
 
 func speed_sort(a, b):
 	return a.speed > b.speed
 
 func _ready() -> void:
 	actors.sort_custom(speed_sort)
-	print(actors)
 
 func _process(delta: float) -> void:
 	pass
