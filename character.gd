52736408c4288a5dfdd4213970b195a1421d8ddf
flowerschaos <cfactionbackup@gmail.com>
2025-03-19 09:57:53 -0400

    gonna be real i'm just screwing around /w plugins

--- character.gd
+++ character.gd
@@ -2,15 +2,10 @@ extends Node
 class_name Character
 
 @export_range(1,10) var endurance: int = 5
-var init_value
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-func init_roll():
-	var result_sum: int = Dice.roll(2, 6)
-	init_value = result_sum
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
