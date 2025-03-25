40e4287e6469d8a115b571839cc13ab8587bad04
flowerschaos <cfactionbackup@gmail.com>
2025-03-25 09:05:09 -0400

    proper switching through turns

--- combat.gd
+++ combat.gd
@@ -1,15 +1,20 @@
 extends Node3D
 
 @onready var turnswitch: Button = $turnswitch
-var cti: int = 0
+
 @onready var curr_turn = InitiativeManager.actors[cti]
+var cti: int
 
 func _ready() -> void:
+	cti = 0
 	print(InitiativeManager.actors)
+	print(curr_turn)
 
 func _on_turnswitch_pressed() -> void:
-	cti += 1
-	print(curr_turn)
-	if cti > InitiativeManager.actors.size():
+	if cti < (InitiativeManager.actors.size()-1):
+		cti += 1
+		curr_turn = InitiativeManager.actors[cti]
+	else:
 		cti = 0
-	curr_turn = InitiativeManager.actors[cti]
+		curr_turn = InitiativeManager.actors[0]
+	print(curr_turn)
