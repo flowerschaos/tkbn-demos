cd261083bbeca6ccf7354a96978d47a091ee77e1
flowerschaos <cfactionbackup@gmail.com>
2025-03-26 08:47:06 -0400

    ALL I WANTED WAS SOME EMPTY SPACES

--- initiative_visualizer.gd
+++ initiative_visualizer.gd
@@ -9,6 +9,8 @@ func _on_combat_combat_start() -> void:
 		var actor_frame = timeline_panel.instantiate()
 		actor_frame.icon.texture = actor.battle_icon
 		add_child(actor_frame)
+	while InitiativeManager.actors.size() < 8:
+		timeline_panel.instantiate()
 
 func _on_turnswitch_pressed() -> void:
 	move_child(get_child(0),InitiativeManager.actors.size())
