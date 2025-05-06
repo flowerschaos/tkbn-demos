0199cfd7f0a903af8476c5d275b79d33a138b969
flowerschaos <cfactionbackup@gmail.com>
2025-05-05 21:47:42 -0400

    properly detects interactable items

--- components/interaction_component.gd
+++ components/interaction_component.gd
@@ -2,7 +2,6 @@ extends ShapeCast3D
 
 func _process(delta: float) -> void:
 	if is_colliding():
-		print("HELLO???")
 		for i in range(get_collision_count()):
 			if get_collider(i).is_in_group("interactable"):
 				print("HEY I HIT AN INTERACTABLE OBJECT")
