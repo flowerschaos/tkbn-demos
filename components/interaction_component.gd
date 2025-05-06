dc37a614f228ebc07ac48e9d59bb337554ffd524
flowerschaos <cfactionbackup@gmail.com>
2025-05-05 21:43:45 -0400

    functional shapecast

--- components/interaction_component.gd
+++ components/interaction_component.gd
@@ -1,3 +1,8 @@
-extends Area3D
-class_name InteractionComponent
+extends ShapeCast3D
 
+func _process(delta: float) -> void:
+	if is_colliding():
+		print("HELLO???")
+		for i in range(get_collision_count()):
+			if get_collider(i).is_in_group("interactable"):
+				print("HEY I HIT AN INTERACTABLE OBJECT")
