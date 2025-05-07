ec3c78e1feb57f97b799edbf8649aab9a0cfee76
flowerschaos <cfactionbackup@gmail.com>
2025-05-07 12:00:33 -0400

    trying to figure out dialogue

--- components/interaction_component.gd
+++ components/interaction_component.gd
@@ -1,7 +1,16 @@
 extends ShapeCast3D
 
+signal item_found
+signal ended_interaction
+
 func _process(delta: float) -> void:
 	if is_colliding():
+		var collider = get_collider(0)
 		for i in range(get_collision_count()):
 			if get_collider(i).is_in_group("interactable"):
-				print("HEY I HIT AN INTERACTABLE OBJECT")
+				Dialogic.start("testbox")
+				item_found.emit()
+
+func _on_timeline_ended():
+	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
+	ended_interaction.emit()
--- overworld/the_beast.gd
+++ overworld/the_beast.gd
@@ -1,22 +1,29 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
+
+var can_move: bool = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if can_move == true:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir := Input.get_vector("left", "right", "forward", "backward")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+		move_and_slide()
 
-	move_and_slide()
+func _on_interaction_component_item_found() -> void:
+	can_move = false
+
+func _on_interaction_component_ended_interaction() -> void:
+	can_move = true
