8f8506bf19042c65974bda290e3859f07a789f18
flowerschaos <cfactionbackup@gmail.com>
2025-06-04 09:50:00 -0400

    skill info

--- combat/skillResource.gd
+++ combat/skillResource.gd
@@ -3,13 +3,10 @@ class_name Skill
 
 @export var name: String
 @export var target: TARGET_TYPE
-@export var type: SKILL_TYPE
+@export var does_damage: bool = false
+@export var damage_amount: int
+@export var has_cooldown: bool = false
+@export var cooldown_timer: int
+@export var is_passive: bool
 
 enum TARGET_TYPE {ENEMIES,ALLIES}
-
-enum SKILL_TYPE {PHYSICAL, RADIOACTIVE}
-
-static func filter_type(type:SKILL_TYPE,array_element:Skill):
-	if array_element.type == type:
-		return true
-	return false
