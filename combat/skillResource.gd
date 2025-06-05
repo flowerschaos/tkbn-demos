8e10bbb1a93ef394ba381ef86263b5090d133366
flowerschaos <cfactionbackup@gmail.com>
2025-06-04 23:02:56 -0400

    more work on skills

--- combat/skillResource.gd
+++ combat/skillResource.gd
@@ -5,8 +5,6 @@ class_name Skill
 @export var target: TARGET_TYPE
 @export var does_damage: bool = false
 @export var damage_amount: int
-@export var has_cooldown: bool = false
-@export var cooldown_timer: int
 @export var is_passive: bool
 
 enum TARGET_TYPE {ENEMIES,ALLIES}
